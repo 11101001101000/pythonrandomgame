58d72f2f58d22bdf492cdca57d832dcc6dc6f55a
11101001101000 <t-h-0928@naver.com>
2022-01-23 17:07:16 +0000

    added launcher

---
+++ launcher.py
@@ -0,0 +1,10 @@
+import os
+a = input ("language(En or Kr)")
+if a == "Kr":
+    os.system("python3 game.py")
+if a == "En":
+    os.system("python3 game_1.py")
+if a == "kr":
+    os.system("python3 game.py")
+if a == "en":
+    os.system("python3 game_1.py")
